56041277a9ff919c003718ec30050d62a2d60350
Iain Cattermole <iain_c@live.co.uk>
2021-12-03 12:34:29 +0000

    Create template for challenges

---
+++ template.py
@@ -0,0 +1,26 @@
+from utils import read_input
+
+
+##########
+# PART 1 #
+##########
+
+
+def part_one():
+    pass
+
+
+##########
+# PART 2 #
+##########
+
+
+def part_two():
+    pass
+
+
+if __name__ == '__main__':
+    print("Part 1")
+    part_one()
+    # print("Part 2")
+    # part_two()
