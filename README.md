6450343b3f7fa5f0c6890117fe7296ae70809699
Iain Cattermole <iain_c@live.co.uk>
2021-12-03 12:23:55 +0000

    Setup repo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# advent-of-code-2021
+# Advent of Code 2021
+
+My solutions to [Advent of Code 2021](https://adventofcode.com/2021/).
---
+++ utils.py
@@ -0,0 +1,16 @@
+from typing import List, Type, Generator
+
+
+def read_input(fname: str, types: List[Type] = None) -> Generator:
+    with open(fname) as f:
+        lines = (line.strip().split() for line in f.readlines())
+    
+    if types is None:
+        return lines
+    else:
+        if len(types) > 1:
+            for line in lines:
+                yield tuple(typ(val) for typ, val in zip(types, line))
+        else:
+            for line in lines:
+                yield types[0](line[0])
