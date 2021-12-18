cb625ac0c81944a4828aae1eb2de5a786cf7a610
Iain Cattermole <iain_c@live.co.uk>
2021-12-18 18:13:11 +0000

    Day 18 solutions

--- day18/snailfish.py
+++ day18/snailfish.py
@@ -1,15 +1,16 @@
 from typing import Optional
 from utils import read_input, run
 from json import loads
-from dataclasses import dataclass
+from math import floor, ceil
+from functools import cached_property
+from itertools import permutations
 
 
 fname = "day18/input.txt"
 
 
 def parse(fname):
-    return [loads(line) for line in read_input(fname, types=[str])]
-
+    return [Node(loads(line)) for line in read_input(fname, types=[str])]
 
 
 class Node:
@@ -22,8 +23,17 @@ class Node:
         elif isinstance(data, list):
             left_data, right_data = data
             self.value = None
-            self.left = Node(left_data)
-            self.right = Node(right_data)
+            self.left = Node(left_data, self)
+            self.right = Node(right_data, self)
+
+    @cached_property
+    def depth(self) -> int:
+        d = 0
+        current = self
+        while not current.is_top():
+            current = current.parent
+            d += 1
+        return d
 
     def is_top(self) -> bool:
         return self.parent is None
@@ -36,21 +46,126 @@ class Node:
     
     def is_right(self) -> bool:
         return not self.is_top() and self.parent.right is self
+    
+    def reduce(self) -> None:
+        while (node := self.find_next_invalid()):
+            if node.is_regular_number():
+                node.split()
+            else:
+                node.explode()
+    
+    def find_next_invalid(self) -> Optional['Node']:
+        for node in self:
+            if not node.is_regular_number() and node.depth >= 4:
+                return node
+        for node in self:
+            if node.is_regular_number() and node.value >= 10:
+                return node
+        return None
+    
+    def is_tree_valid(self) -> bool:
+        return all(node.is_node_valid() for node in self)
+    
+    def is_node_valid(self) -> bool:
+        if self.is_regular_number():
+            return self.value < 10
+        else:
+            return self.depth < 4  
+    
+    def magnitude(self) -> int:
+        if self.is_regular_number():
+            return self.value
+        
+        left_mag = self.left.magnitude()
+        right_mag = self.right.magnitude()
+        return left_mag * 3 + right_mag * 2
 
     def explode(self) -> None:
-        pass
+        assert self.left.is_regular_number()
+        assert self.right.is_regular_number()
+        
+        l_node = self.next_left_regular_number()
+        if l_node:
+            l_node.value += self.left.value
+        
+        r_node = self.next_right_regular_number()
+        if r_node:
+            r_node.value += self.right.value
+        
+        self.value = 0
+        self.left = None
+        self.right = None
+
+    def next_left_regular_number(self):      
+        current = self
+        # Traverse up until can go left
+        while current.is_left():
+            current = current.parent
+        # If reached the top then we're in the leftmost node already (discard)
+        if current.is_top():
+            return None
+        # Hop left one step
+        current = current.parent.left
+        # Traverse down right to find the adjacent node
+        while not current.is_regular_number():
+            current = current.right
+        return current
+
+    def next_right_regular_number(self):        
+        current = self
+        # Traverse up until can go right
+        while current.is_right():
+            current = current.parent
+        # If reached the top then we're in the rightmost node already (discard)
+        if current.is_top():
+            return None
+        # Hop right one step
+        current = current.parent.right
+        # Traverse down left to find the adjacent node
+        while not current.is_regular_number():
+            current = current.left
+        return current
 
     def split(self) -> None:
         if self.is_regular_number():
-            pass
+            half = self.value / 2
+            left = floor(half)
+            right = ceil(half)
+            self.left = Node(left, self)
+            self.right = Node(right, self)
+            self.value = None
+    
+    def to_primitive(self):
+        if self.is_regular_number():
+            return self.value
+        else:
+            return [
+                self.left.to_primitive(),
+                self.right.to_primitive(),
+            ]
+
+    def __add__(self, other) -> 'Node':
+        if not isinstance(other, Node):
+            raise NotImplementedError()
+        left = self.to_primitive()
+        right = other.to_primitive()
+        result = Node([left, right])
+        result.reduce()
+        return result
+    
+    def __radd__(self, other) -> 'Node':
+        if other == 0:
+            return self
+        raise NotImplementedError()   
+
+    def __iter__(self):
+        yield self
+        if not self.is_regular_number():
+            yield from iter(self.left)
+            yield from iter(self.right)
     
     def __repr__(self) -> str:
-        if self.is_regular_number():
-            return repr(self.value)
-        else:
-            return repr([self.left, self.right])
-
-
+        return repr(self.to_primitive())
 
 
 ##########
@@ -58,14 +173,9 @@ class Node:
 ##########
 
 
-def snail_add(n1, n2):
-    result = [n1, n2]
-
-
 def part_one():
-    data = parse(fname)
-    tree = Node(data)
-    print(tree)
+    snail_numbers = parse(fname)
+    return sum(snail_numbers).magnitude()
 
 
 ##########
@@ -74,7 +184,9 @@ def part_one():
 
 
 def part_two():
-    pass
+    snail_numbers = parse(fname)
+    result = max(sum(pair).magnitude() for pair in permutations(snail_numbers, 2))
+    return result
 
 
 if __name__ == '__main__':
