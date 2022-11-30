3a2952f38891a40ee43716bee3636daf136d4690
Iain Cattermole <iaincattermole@skyscanner.net>
2022-11-30 22:45:41 +0000

    Create 2022 template

---
+++ 2022/00_template.py
@@ -0,0 +1,27 @@
+from utils import read_input, run
+
+
+fname = "input/00_template.txt"
+
+
+##########
+# PART 1 #
+##########
+
+
+def part_one():
+    data = read_input(fname)
+    print(data)
+
+
+##########
+# PART 2 #
+##########
+
+
+def part_two():
+    pass
+
+
+if __name__ == '__main__':
+    run(part_one, part_two)
---
+++ 2022/utils.py
@@ -0,0 +1,31 @@
+from timeit import default_timer as timer
+from typing import Callable, TypeVar, Any
+
+
+T = TypeVar('T')
+
+
+def default_parse_line(line: str) -> list[str]:
+    return line.strip().split()
+
+
+def read_input(fname: str, parse_line: Callable[[str], T] = default_parse_line) -> list[T]:
+    with open(fname) as f:
+        return [parse_line(line) for line in f.read().splitlines()]
+
+
+def timed(f, *args, **kwargs):
+    t1 = timer()
+    result = f(*args, **kwargs)
+    t2 = timer()
+    return result, t2 - t1
+
+
+def run(part_one, part_two):
+    print("PART 1")
+    result, time = timed(part_one)
+    print(f"Answer:\t{result}\nTime:\t{time*1000}ms")
+    print()
+    print(f"PART 2")
+    result, time = timed(part_two)
+    print(f"Answer:\t{result}\nTime:\t{time*1000}ms")
