b3bbcadc5ec2371b62021ff4b0da3d0c62c4a85c
Iain Cattermole <iaincattermole@skyscanner.net>
2022-12-29 18:46:16 +0000

    Finish day 22

--- 2022/22/monkey_map.py
+++ 2022/22/monkey_map.py
@@ -14,6 +14,7 @@ DOWN = (1, 0)
 LEFT = (0, -1)
 UP = (-1, 0)
 
+CUBE_SIZE = 50
 DIRECTIONS = [RIGHT, DOWN, LEFT, UP]
 
 
@@ -43,14 +44,14 @@ def find_start(grid):
             return (0, i)
 
 
-def add(grid, pos, direction):
+def add_and_wrap(grid, pos, direction):
     return tuple((a + b) % grid.shape[i] for i, (a, b) in enumerate(zip(pos, direction)))
 
 
 def next_tile(grid, pos, direction):
-    tile = add(grid, pos, direction)
+    tile = add_and_wrap(grid, pos, direction)
     while grid[tile] == VOID:
-        tile = add(grid, tile, direction)
+        tile = add_and_wrap(grid, tile, direction)
     return tile
 
 
@@ -64,7 +65,7 @@ def move(grid, pos, dist, direction):
     return new_pos
 
 
-def rotate(direction, rot):
+def rotate_direction(direction, rot):
     return DIRECTIONS[(DIRECTIONS.index(direction) + rot) % 4]
 
 
@@ -76,7 +77,7 @@ def part_one(input_file):
 
     for dist, rot in steps:
         pos = move(grid, pos, dist, direction)
-        direction = rotate(direction, rot)
+        direction = rotate_direction(direction, rot)
         print(f"Moved {dist}, rotated {rot}, {direction=} {pos=}")
 
     row, col = pos
@@ -89,46 +90,82 @@ def part_one(input_file):
 ##########
 
 
+def add(pos, direction):
+    return tuple(a + b for a, b in zip(pos, direction))
+
+
 def cube_face(pos):
-    return tuple(a // 50 for a in pos)
+    return tuple(a // CUBE_SIZE for a in pos)
 
 
-def wrap_direction(current_face, next_face):
-    
-    if current_face == (0, 2):
-        if next_face == (0, 0):
-            return LEFT
-        if next_face == (1, 2):
-            return LEFT
-        if next_face == (2, 2):
-            return UP
-    
-    if current_face == (0, 1):
-        if next_face == (0, 0):
-            return 
-        if next_face == (2, 1):
-            return
-    
-    if current_face == (1, 1):
-        if next_face == (1, 0):
-            return
-        if next_face == (1, 2):
-            return
-    
-    # if current_face 
+def cube_face_to_real(face):
+    return tuple(a * CUBE_SIZE for a in face)
 
 
-def wrap_position(current_face, next_face, pos):
-    pass
+def cube_position(pos):
+    return tuple(a % CUBE_SIZE for a in pos)
+
+
+def in_grid(grid, coord):
+    return all(0 <= coord[i] < grid.shape[i] for i in range(2))
+
+
+def rotate_cube_position(pos, rots):
+    for _ in range(rots):
+        x, y = pos
+        pos = (y, CUBE_SIZE - 1 - x)
+    return pos
+
+
+# TODO: need current face as well
+WRAP_FACE = {
+    (-1, 1): ((3, 0), 1),
+    (-1, 2): ((3, 0), 0),
+    (0, 0): ((2, 0), 2),
+    (0, 3): ((2, 1), 2),
+    (1, 0): ((2, 0), 3),
+    (1, 2): ((0, 2), 3),
+    (2, -1): ((0, 1), 2),
+    (2, 2): ((0, 2), 2),
+    (3, -1): ((0, 1), 3),
+    (3, 1): ((2, 1), 3),
+    (4, 0): ((0, 2), 0),
+}
+
+
+def wrap_face(current_face, next_face):
+    match (current_face, next_face):
+        case _, (-1, 1): return ((3, 0), 1)
+        case _, (-1, 2): return ((3, 0), 0)
+        case _, (0, 0): return ((2, 0), 2)
+        case _, (0, 3): return ((2, 1), 2)
+        case (1, 1), (1, 0): return ((2, 0), 3)
+        case (2, 0), (1, 0): return ((1, 1), 1)
+        case (1, 1), (1, 2): return ((0, 2), 3)
+        case (0, 2), (1, 2): return ((1, 1), 1)
+        case _, (2, -1): return ((0, 1), 2)
+        case _, (2, 2): return ((0, 2), 2)
+        case _, (3, -1): return ((0, 1), 3)
+        case (3, 0), (3, 1): return ((2, 1), 3)
+        case (2, 1), (3, 1): return ((3, 0), 1)
+        case _, (4, 0): return ((0, 2), 0)
+        case _: raise Exception()
+
+
+def wrap_position(prev_face, pos, direction):
+    relative_pos = cube_position(pos)
+    face = cube_face(pos)
+    next_face, rots = wrap_face(prev_face, face)
+    new_pos = add(cube_face_to_real(next_face), rotate_cube_position(relative_pos, rots))
+    new_dir = rotate_direction(direction, rots)
+    # print(f'Wrapping {pos=} {face=} {direction=} to {new_pos=} {new_dir=}')
+    return new_pos, new_dir
 
 
 def next_cube_tile(grid, pos, direction):
-    tile = add(grid, pos, direction)
-    if grid[tile] == VOID:
-        current_face = cube_face(pos)
-        next_face = cube_face(tile)
-        
-        direction = 
+    tile = add(pos, direction)
+    if not in_grid(grid, tile) or grid[tile] == VOID:
+        tile, direction = wrap_position(cube_face(pos), tile, direction)
         
     return tile, direction
 
@@ -152,11 +189,10 @@ def part_two(input_file):
 
     for dist, rot in steps:
         pos, direction = move_on_cube(grid, pos, dist, direction)
-        direction = rotate(direction, rot)
-        print(f"Moved {dist}, rotated {rot}, {direction=} {pos=}")
+        direction = rotate_direction(direction, rot)
+        # print(f"Moved {dist}, rotated {rot}, {direction=} {pos=}")
 
     row, col = pos
-
     return (row + 1) * 1000 + (col + 1) * 4 + DIRECTIONS.index(direction)
 
 
